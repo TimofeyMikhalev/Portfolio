b91d4b1d2f50184537083e6bb04b9b7eb3960b21
TimofeyMikhalev <79932412+TimofeyMikhalev@users.noreply.github.com>
2025-02-16 01:20:35 +0200

    today repeat object & array

--- Portfolio/weather/main.js
+++ Portfolio/weather/main.js
@@ -59,4 +59,229 @@ const eaglesParrots = allAnimals.filter(a => a === 'орёл' || a === 'попу
 const treatedAnimals = eaglesParrots.map(a => `${a}`)
 console.log(treatedAnimals)
 
+
 //3
+const vehicles = ["M", "V", "A", "M", "M", "S", "V", "V", "S", "M"];
+
+const isLandTransport = (vehicle) => {
+    const isLandTransport = ["V", "V", "S", "M"];
+
+    return isLandTransport.includes(vehicle);
+};
+
+const allLandTransport = vehicles.every(isLandTransport);
+console.log(allLandTransport); // false, в массиве есть ✈️ и ?
+
+
+//4
+const foodDescriptions = [
+    "Салат Цезарь",
+    "Паста Болоньезе",
+    "Ролл Филадельфия",
+    "Суп Том Ям",
+    "Стейк Медиум",
+    "Пицца Маргарита",
+    "Чизкейк Нью-Йорк",
+    "Хот-дог с сыром",
+    "Суши Филадельфия",
+    "Фруктовый салат",
+];
+
+const lengthStr = (str) => {
+    return str.length
+}
+
+
+let lenFood = foodDescriptions.sort((a, b) => {
+    return lengthStr(a) - lengthStr(b)
+})
+
+
+
+
+//5
+const fruits = ["яблоко", "банан", "киви", "манго", "груша"];
+
+const sumFruits = fruits.reduce((a, b) => {
+    return a + b.length
+}, 0)
+
+
+
+//6
+const number = [2, 4, 7, 1, 9, 4, 12, 7, 98, 7]
+
+const newNumber = number.filter((a) => {
+    if(a % 2 === 0) {
+        return a
+    }
+})
+
+
+
+
+//Object
+
+let person1 = {name: "Alex", age: 30};
+let person2 = {name: "Alex", age: 30};
+
+
+function isDeepEqual(obj1, obj2) {
+
+    if (obj1 === obj2) {
+        return true;
+    }
+
+    if (obj1 === null || obj2 === null || typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+        return false;
+    }
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+  
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+
+    for (let key of keys1) {
+            if (!keys2.includes(key) || !isDeepEqual(obj1[key], obj2[key])) {
+
+            return false;
+        }
+
+    }
+
+    return true;
+
+};
+
+
+
+const itemsInCart = [
+    { product: 'Носки', quantity: 3 },
+    { product: 'Штаны', quantity: 1 },
+    { product: 'Кепка', quantity: 1 },
+]
+
+let aOne = structuredClone(itemsInCart)
+
+aOne[0].product = 'Sick'
+
+
+let cat = {
+    name: 'Tom',
+    isHunter: true
+}
+  
+let mouse = {
+    name: 'Jerry',
+    isHunter: false
+}
+  
+cat.isHunter = mouse.isHunter;//cat - isHunter: false && mouse - isHunter: false
+mouse.isHunter = undefined; //cat - isHunter: false && mouse - isHunter: undefined
+mouse = cat; //cat - isHunter: false && mouse - isHunter: false
+
+// console.log(mouse.isHunter);//false
+// console.log(cat.isHunter);//false
+
+
+
+function Animal () { }
+
+const animal = Animal() // ❌
+console.log(animal) // undefined
+
+
+const animalTwo = new Animal() // ✅
+
+// console.log(animal)
+// console.log(animalTwo)
+
+
+
+
+//Object.keys, values, entries
+
+// let user = {
+//     name: "John",
+//     age: 30
+// };
+
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+// for(let value of Object.values(user)) {
+//     console.log(value)
+// }
+
+
+let prices = {
+    banana: 1,
+    orange: 2,
+    meat: 4,
+};
+
+//чтобы преобразовать его обратно в объект.
+let doublePrises = Object.fromEntries(
+    // преобразовать в массив, затем map, затем fromEntries (обратно в объект)
+    Object.entries(prices).map(([key, value]) => [ key, value * 2 ])
+)
+
+// console.log(doublePrises)
+
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+// function sumSalaries(salaries) {
+//     let sum = 0
+    
+//     for(let key of Object.values(salaries)) {
+//        sum += key
+//     }
+
+//     return sum
+// }
+
+// reduce перебирает массив значений salaries,
+// складывает их
+// и возвращает результат
+// function sumSalaries(salaries) {
+//     return Object.values(salaries).reduce((a, b) => a + b, 0) // 650
+// }
+
+// let user = {
+//     name: 'John',
+//     age: 30
+// };
+
+// function count(obj) {
+//     return Object.keys(obj).length
+// }
+  
+// console.log(count(user)); // 2
+
+
+
+const makePizza = function(title, cd) {
+    console.log(`Заказ получен ${title}`)
+    setTimeout(cd, 3000)
+}
+
+const readBooks = function() {
+    console.log('Читаю книгу «Колдун и кристалл»…');
+}
+
+const eatPizza = function() {
+    console.log('Ура! Пицца готова, пора подкрепиться.');
+}
+
+makePizza('Piperonia', eatPizza)
+readBooks()
